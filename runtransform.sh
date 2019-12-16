ab55a1f6554809d9ad152cefc38a15f347a1f676
Glenn Walker <glennwalker3976@gmail.com>
2019-12-16 13:04:14 +0000

    Update runtransform.sh
    Latest dates for Dec 2019.

--- runtransform.sh
+++ runtransform.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export DATA_FILE="data/November 2019/Report-20191120082825.csv"
-export OUT_FILE="data/November 2019/Transform-Report-20191120082825.csv"
+export DATA_FILE="data/December 2019/Report-20191216130214.csv"
+export OUT_FILE="data/December 2019/Transform-Report-20191216130214.csv"
 ./run.sh
