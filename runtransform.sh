cc17f82b05939097597dfd76a2713bdfccc757e6
Glenn Walker <glennwalker3976@gmail.com>
2020-02-16 14:44:13 +0000

    Update runtransform.sh

--- runtransform.sh
+++ runtransform.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export DATA_FILE="data/December 2019/Report-20191216130214.csv"
-export OUT_FILE="data/December 2019/Transform-Report-20191216130214.csv"
+export DATA_FILE="data/February 2020/Report-20200212092443.csv"
+export OUT_FILE="data/February 2020/Transform-Report-20200212092443.csv"
 ./run.sh
