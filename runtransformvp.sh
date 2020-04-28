5a227cf1cc466534f3d110a922ef3acfc30ec6aa
Glenn Walker <glennwalker3976@gmail.com>
2020-04-28 10:07:34 +0100

    Update runtransformvp.sh
    Latest filenames.

--- runtransformvp.sh
+++ runtransformvp.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export DATA_FILE="data/villagepeople/April 2020/Report-20200402094329.csv"
-export OUT_FILE="data/villagepeople/April 2020/Transform-Report-20200402094329.csv"
+export DATA_FILE="data/villagepeople/April 2020/Report-20200428085040.csv"
+export OUT_FILE="data/villagepeople/April 2020/Transform-Report-20200428085040.csv"
 ./run.sh
